9c03c0bc17023252483815a787e68d74e33c3db2
Dave Brotherstone <davegb@pobox.com>
2009-08-07 22:02:09 +0100

    [FIXED] Update to PathRemoveFileSpec(generic_string) to match the TCHAR version
    - This fixes a few test cases where the generic_string version didn't match what the TCHAR version did.
      - Added a tests directory to PowerEditor, with the tests run for this function.
        The tests are currently just asserts in a function, with no header - we'll convert these
        to whichever unit test framework is implemented.
    
    Signed-off-by: Dave Brotherstone <davegb@pobox.com>

---
+++ tests/testPathRemoveFileSpec.cpp
@@ -0,0 +1,104 @@
+/* This file was originally hacked into winmain.cpp to run the tests
+   Hopefully it'll be converted to whatever unit testing framework is in place, at some point
+*/
+
+void testPathRemoveFileSpec(TCHAR* charPath, generic_string strPath)
+{
+	PathRemoveFileSpec(strPath);
+	PathRemoveFileSpec(charPath);
+	assert(strPath == charPath);
+}
+
+void testPathFunctions()
+{
+	generic_string strPath;
+	TCHAR charPath[MAX_PATH];
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\foo\\bar.ext"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\foo\\bar\\test.php"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\foo\\"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\foo"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\foo\\\\test"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:\\"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:bar.ext"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C:"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("\\test\\foo\\bar.ext"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("\\foo\\bar.ext"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("\\foo\\"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("\\foo"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("foo\\bar.ext"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("foo.exe"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("foo"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C://foo//test.exe"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("//foo//test.exe"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C://foo//test"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C://foo//"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C://foo"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+	_tcscpy_s(charPath, MAX_PATH, _T("C://"));
+	strPath = charPath;
+	testPathRemoveFileSpec(charPath, strPath);
+
+}
