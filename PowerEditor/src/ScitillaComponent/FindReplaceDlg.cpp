391526c05509f1e156924d13b5647758cc57825a
Jocelyn Legault <jocelynlegault@gmail.com>
2009-10-19 23:36:58 -0600

    Docking Find in Files: Fixed the hanging bug
    - Fixed the hanging bug and at least another less serious bug, where the Find in Files window simply did not appear.
    
    [#1 state:needs_hack]
    
    Signed-off-by: Jocelyn Legault <jocelynlegault@gmail.com>

--- PowerEditor/src/ScitillaComponent/FindReplaceDlg.cpp
+++ PowerEditor/src/ScitillaComponent/FindReplaceDlg.cpp
@@ -1730,15 +1730,12 @@ void FindReplaceDlg::findAllIn(InWhat op)
 	if (::SendMessage(_hParent, cmdid, 0, 0))
 	{
 		wsprintf(_findAllResultStr, TEXT("%d hits"), _findAllResult);
-		if (_findAllResult)
-		{
-			focusOnFinder();
-		}
-		else
-			getFocus(); // no hits
+		focusOnFinder();
 	}
 	else // error - search folder doesn't exist
+	{
 		::SendMessage(_hSelf, WM_NEXTDLGCTL, (WPARAM)::GetDlgItem(_hSelf, IDD_FINDINFILES_DIR_COMBO), TRUE);
+	}
 }
 
 
