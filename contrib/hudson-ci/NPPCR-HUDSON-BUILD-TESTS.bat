cdf4993b24e6455a361a79639f090e94aeab8836
Thell Fowler <git@tbfowler.name>
2010-02-02 13:26:40 -0600

    NPPCR-HUDSON-BUILD-TESTS.bat: add leakdetect switch
    - This patch makes use of the NPPCR leakdetect switch to enable auto
      exit after notepad_plus initializes and emits the NPPN_READY msg
      so that we can get a more accurate memory leak detection check.
    
    - This patch is on top of patches from [#33]
    
    [#25]
    
    Signed-off-by: Thell Fowler <git@tbfowler.name>

--- contrib/hudson-ci/NPPCR-HUDSON-BUILD-TESTS.bat
+++ contrib/hudson-ci/NPPCR-HUDSON-BUILD-TESTS.bat
@@ -1,54 +1,66 @@
-echo off
+ECHO off
+REM  Hudson to MSBuild script for N++CR
 
-::-------------------------------------------------
-:: This script passes values from the hudson matrix
-:: project to msbuild.
-::-------------------------------------------------
+:: Google TEST and Mock paths
+if NOT [%NPPCR_GTEST_STYLE%]==[ENVIRONMENT] (
+  set GOOGLE_TEST_DIR=
+  set GOOGLE_MOCK_DIR=
+) else (
+	if NOT [%GOOGLE_TEST_DIR:~-1%]==[\] set GOOGLE_TEST_DIR = %GOOGLE_TEST_DIR%\
+	if NOT [%GOOGLE_MOCK_DIR:~-1%]==[\] set GOOGLE_MOCK_DIR = %GOOGLE_MOCK_DIR%\
+)
 
-:: Set the target solution from the Hudson passed var.
-:: NPPCR Solution files use only the VC major version
-:: Leave early if the solution doesn't exist.
+:: Target solution name from the build's VC version
 set VER_MINOR=%NPPCR_BUILD_VC_VER:*.=%
 set VER_MINOR=.%VER_MINOR%
 call set VER_MAJOR=%%NPPCR_BUILD_VC_VER:%VER_MINOR%=%%
 set SOLUTION=notepadPlus.%VER_MAJOR%.sln
-if NOT EXIST "%NPPCR_ROOT_PATH%%SOLUTION%" exit 1
+echo Build test target solution:: %SOLUTION%
 
-:: Remove anything that may be in the bin directory
-set NPPCR_BIN_PATH=%NPPCR_ROOT_PATH%PowerEditor\bin\
-if EXIST "%NPPCR_BIN_PATH%" del /q "%NPPCR_BIN_PATH%"
-
-:: Clear environment variables for gtest/mock
-if NOT [%NPPCR_GTEST_STYLE%]==[Environment] (
-  set NPPCR_GTEST_DIR=
-  set NPPCR_GTEST_DIR=
-)
-
-:: Setup the build environment
+:: VC environment from the build's VC version
 set VC_PATH_VER=%NPPCR_BUILD_VC_VER:.=%
 set VC_VARS=VS%VC_PATH_VER%COMNTOOLS
 call set VC_VARS=%%%VC_VARS%%%..\..\VC\vcvarsall.bat
-call "%VC_VARS%" %NPPCR_BUILD_ARCH%
+echo Build test VC variable target:: %VC_VARS%
 
-:: Build the msbuild command
+:: MSBuild command
 set MSBUILD_OPTS=/v:detailed /t:rebuild /p:Configuration
 set MSBUILD_OPTS=%MSBUILD_OPTS%=%NPPCR_BUILD_CONFIGURATION%
-call msbuild %MSBUILD_OPTS% "%NPPCR_ROOT_PATH%%SOLUTION%"
-if ERRORLEVEL 1 exit %ERRORLEVEL%
+echo Build test MSBuild target:: %MSBUILD_OPTS%
 
-:: Only check debug builds for memory leaks
+:: Console debugger command
 set NPPCR_BIN_PATH=%NPPCR_ROOT_PATH%PowerEditor\bin
 set NPPCR_BIN=%NPPCR_BIN_PATH%\notepad++Debug.exe
-if NOT EXIST "%NPPCR_BIN%" exit 0
-
-:: Build the console debugger command
 set CDB_PATH=%MS_DBGTOOLS_PATH%cdb.exe
 set CDB_ARGS=-kqm -g -G -y
-set NPPCR_DBG_CMD="%CDB_PATH%" %CDB_ARGS% "%NPPCR_BIN_PATH%" "%NPPCR_BIN%"
+set NPPCR_DBG_CMD="%CDB_PATH%" %CDB_ARGS% "%NPPCR_BIN_PATH%" "%NPPCR_BIN%" -leakdetect
+echo Build test Mem leak target:: %NPPCR_DBG_CMD%
 
-:: Call once for output and then to check output
+:: Main routine
+if NOT EXIST "%NPPCR_ROOT_PATH%%SOLUTION%" exit 1
+call "%VC_VARS%" %NPPCR_BUILD_ARCH%
+if %ERRORLEVEL% EQU 0 call msbuild %%MSBUILD_OPTS%% "%NPPCR_ROOT_PATH%%SOLUTION%"
+if NOT [%NPPCR_BUILD_CONFIGURATION%]==[Debug] EXIT %ERRORLEVEL%
+if NOT %ERRORLEVEL% EQU 0 (
+	call :CLEAN_UP
+	exit 1
+)
+
 call %NPPCR_DBG_CMD%
-call %NPPCR_DBG_CMD%|FINDSTR /C:"Detected memory leaks"
-if %ERRORLEVEL% EQU 0 exit 1
+if NOT %ERRORLEVEL% EQU 0 (
+	call :CLEAN_UP
+	exit 1
+)
 
-exit %ERRORLEVEL%
+call %NPPCR_DBG_CMD%|FINDSTR /C:"Detected memory leaks"
+if %ERRORLEVEL% EQU 1 (
+	call :CLEAN_UP
+	exit 0
+) else (
+	call :CLEAN_UP
+	exit 1
+)
+
+:CLEAN_UP
+call msbuild /t:clean "%NPPCR_ROOT_PATH%%SOLUTION%"
+goto :EOF
--- contrib/hudson-ci/README.txt
+++ contrib/hudson-ci/README.txt
@@ -40,12 +40,14 @@ Setup
 	* Download hudson.war from http://hudson-ci.org/ and launch it by
 	  executing java -jar hudson.war then visit http://localhost:8080/
 	* Follow the 'Manage Hudson' link to 'Manage Plugins' -> 'Available'
-	  and select the 'Hudson Setenv Plugin' for installation.  Click the
+	  and select the 'Setenv Plugin' for installation.  Click the
 	  'install' button at the bottom of the page.
 	* Close down Hudson and restart. (ctrl-c in the console running the .war)
-	* On the Hudson main dashboard select 'New Job' and name it N++CR Matrix
+	* On the Hudson main dashboard select 'New Job', select the 'Build multi-
+		configuration project', name it N++CR Matrix, and click 'Ok'.
 	* Close down Hudson again.
-	* Copy the contrib\Hudson-ci\config.xml file to ~\.hudson\jobs\N++CR Matrix\
+	* Copy the contrib\Hudson-ci\config.xml file to .hudson\jobs\N++CR Matrix\ in
+		your home directory.
 	* Restart Hudson
 	* From the main dashboard select the N++CR Matrix job then 'Configure'
 	* Scroll down to the 'Build Environment' section and set the NPPCR_ROOT_PATH
